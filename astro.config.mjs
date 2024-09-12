5112d973df0165aaea6b0b84139708b413ea604e
Giuseppe <giuseppeferrero@hotmail.com>
2024-09-12 14:18:00 +0200

    Cambiado integración astro node por astro cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,17 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import solidJs from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
   integrations: [tailwind(), icon(), solidJs(), mdx()],
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'server',
+  adapter: cloudflare()
 });
